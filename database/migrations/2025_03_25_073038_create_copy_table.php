0349eb723cff396222382f31ff24265285ad7b0f
unknown <nastya_oleinik@list.ru>
2025-03-25 13:31:43 +0500

    Migrations task

---
+++ database/migrations/2025_03_25_073038_create_copy_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('copy', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->unsignedBigInteger('edition_id');
+            $table->foreign('edition_id')->references('id')->on('edition');
+
+            $table->decimal('wear_coefficient', 3, 2)->default(1.00); // Коэффициент износа, по умолчанию 1 (новый), число с 3 знаками и из них 2 после запятой
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('copy');
+    }
+};
---
+++ database/migrations/2025_03_25_074247_create_lending_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lending', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->unsignedBigInteger('copy_id');
+            $table->foreign('copy_id')->references('id')->on('copy');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->date('lending_date'); //дата выдачи
+            $table->date('plan_return_date'); //планируемая дата возврата
+            $table->date('fact_return_date')->nullable(); //фактическая дата возврата (может быть NULL)
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lending');
+    }
+};
